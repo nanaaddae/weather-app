6d5a9f7b2da0d9b56077447fa70df44e62eda2f0
nanaaddae <80304875+nanaaddae@users.noreply.github.com>
2024-01-12 17:12:45 -0500

    Update ZipCodeForm.jsx
    Edited the component that accepted the Zip Code. Also added a way for an error to be displayed whenever the user enters an invalid zip code as well

--- src/components/ZipCodeForm.jsx
+++ src/components/ZipCodeForm.jsx
@@ -2,33 +2,41 @@ import React, { useState } from 'react';
 
 const ZipCodeForm = ({ onFormSubmit }) => {
   const [zipCode, setZipCode] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (zipCode.trim() !== '') {
       try {
-        const response = await fetch(`YOUR_API_ENDPOINT/${zipCode}`);
+        const response = await fetch(`http://localhost:8080/weather/${zipCode}`);// call to the Spring Boot backend
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
         const data = await response.json();
         onFormSubmit(data);
+        setError(''); // Reset the error state when a valid zip code is successfully fetched
       } catch (error) {
         console.error('Error fetching weather data:', error);
-        // Handle the error appropriately
+        setError('Error fetching weather data. Please try again.'); // Set the error state
       }
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Enter Zip Code:
-        <input
-          type="text"
-          value={zipCode}
-          onChange={(e) => setZipCode(e.target.value)}
-        />
-      </label>
-      <button type="submit">Get Weather</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter Zip Code:
+          <input
+            type="text"
+            value={zipCode}
+            onChange={(e) => setZipCode(e.target.value)}
+          />
+        </label>
+        <button type="submit">Get The Weather!</button>
+      </form>
+      {error && <div style={{ color: 'red' }}>{error}</div>} {/* Display the error message */}
+    </div>
   );
 };
 
