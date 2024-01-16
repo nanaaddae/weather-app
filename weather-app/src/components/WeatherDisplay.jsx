d5cf92b313f151dbee7e00cfcada88302cf5870b
nanaaddae <80304875+nanaaddae@users.noreply.github.com>
2024-01-16 15:44:24 -0500

    Update WeatherDisplay.jsx
    Adjusted the weather display component so that the icon will appear now and also now the information now in the card should change when an invalid zip code is entered

--- weather-app/src/components/WeatherDisplay.jsx
+++ weather-app/src/components/WeatherDisplay.jsx
@@ -1,20 +1,35 @@
-
 import React from 'react';
 
 const WeatherDisplay = ({ weatherData }) => {
-  
+  const isValidData = weatherData && !weatherData.error;
+
   return (
-    <div>
-      <h2>Current Weather</h2>
-      {weatherData ? (
-        <div>
-        <p>City Name: {weatherData.name}</p>
-        <p>Temperature: {Math.round((weatherData.main.temp - 273.15) * 9/5 + 32)}°F</p>
-        <p>Conditions: {weatherData.weather[0].description}</p>
+    <div className="container mt-4">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="card-title">Current Weather</h2>
+              {isValidData ? (
+                <div>
+                  <p>City Name: {weatherData.name}</p>
+                  <p>Temperature: {Math.round((weatherData.main.temp - 273.15) * 9/5 + 32)}°F</p>
+                  <p>Conditions: {weatherData.weather[0].description}</p>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                    alt="Weather Icon"
+                    style={{ float: 'right', marginRight: '10px', width: '150px', height: '150px' }}
+                  />
+                </div>
+              ) : (
+                <div>
+                  <p>No weather data available</p>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-      ) : (
-        <p>No weather data available</p>
-      )}
+      </div>
     </div>
   );
 };
